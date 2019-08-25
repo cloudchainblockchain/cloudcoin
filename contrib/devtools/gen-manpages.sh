b9c8b16903b4e2254d188e3103913d287f2222d8
cloudchainblockchain <cloudchainblockchain@gmail.com>
2019-08-25 03:45:46 +0200

    rename

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,23 +7,23 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-LITECOIND=${BITCOIND:-$BINDIR/litecoind}
-LITECOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-LITECOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-LITECOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+CLOUDCOIND=${BITCOIND:-$BINDIR/cloudcoind}
+CLOUDCOINCLI=${BITCOINCLI:-$BINDIR/cloudcoin-cli}
+CLOUDCOINTX=${BITCOINTX:-$BINDIR/cloudcoin-tx}
+CLOUDCOINQT=${BITCOINQT:-$BINDIR/qt/cloudcoin-qt}
 
-[ ! -x $LITECOIND ] && echo "$LITECOIND not found or not executable." && exit 1
+[ ! -x $CLOUDCOIND ] && echo "$CLOUDCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-LTCVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+LTCVER=($($CLOUDCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
 # but has different outcomes for bitcoin-qt and bitcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$LITECOIND --version | sed -n '1!p' >> footer.h2m
+$CLOUDCOIND --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $LITECOIND $LITECOINCLI $LITECOINTX $LITECOINQT; do
+for cmd in $CLOUDCOIND $CLOUDCOINCLI $CLOUDCOINTX $CLOUDCOINQT; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=${LTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
   sed -i "s/\\\-${LTCVER[1]}//g" ${MANDIR}/${cmdname}.1
